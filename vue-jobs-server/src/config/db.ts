6ad5d50287183064b4d15f31bece15c834815720
maxamed <maxamedadan169@gmail.com>
2024-08-11 00:05:02 +0300

    added database

---
+++ vue-jobs-server/src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async (): Promise<void> => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit with a failure code
+    }
+};
+
+export default connectDB;
