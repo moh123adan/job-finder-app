abbe2498331b4323495ae16370df0cc9b5849629
maxamed <maxamedadan169@gmail.com>
2024-08-11 18:06:13 +0300

    added the auth files and the types of it

--- vue-jobs-server/src/index.ts
+++ vue-jobs-server/src/index.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import jobRoutes from './routes/JobRoutes';
 import connectDB from './config/db'
 import colors from 'colors';
+import errorHandler from './middleware/error_handler';
 
 dotenv.config();
 
@@ -14,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }))
+
 
 app.use('/api', jobRoutes);
 
