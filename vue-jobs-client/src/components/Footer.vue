21b62c3137ee423c4421fa28c08fbd18a66c2595
maxamed <maxamedadan169@gmail.com>
2024-08-17 17:06:39 +0300

    added footer

---
+++ vue-jobs-client/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="bg-green-700 text-white py-6">
+    <div
+      class="container mx-auto flex flex-col md:flex-row justify-between items-center"
+    >
+      <p class="text-center md:text-left mb-4 md:mb-0">
+        © 2024 Your Company. All rights reserved.
+      </p>
+      <div class="flex space-x-4">
+        <a href="#" class="text-white hover:text-gray-300">
+          <i class="pi pi-facebook text-2xl"></i>
+        </a>
+        <a href="#" class="text-white hover:text-gray-300">
+          <i class="pi pi-twitter text-2xl"></i>
+        </a>
+        <a href="#" class="text-white hover:text-gray-300">
+          <i class="pi pi-linkedin text-2xl"></i>
+        </a>
+        <a href="#" class="text-white hover:text-gray-300">
+          <i class="pi pi-instagram text-2xl"></i>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+footer {
+  background-color: #047857; /* Tailwind's green-700 */
+}
+</style>
