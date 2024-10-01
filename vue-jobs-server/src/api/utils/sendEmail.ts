b86cc193cfc6ccd237e7f9ead569157cae1220fe
moh123adan <maxamedadan169@gmail.com>
2024-10-01 21:59:58 +0300

    added nodemailer and express async handler

--- vue-jobs-server/src/api/utils/sendEmail.ts
+++ vue-jobs-server/src/api/utils/sendEmail.ts
@@ -4,4 +4,20 @@ import expressAsyncHandler from "express-async-handler";
 interface User {
     firstName: string;
     email: string;
-  }
+}
+
+const sendPasswordResetEmail = expressAsyncHandler(async (user: User, otp: string): Promise<void> => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL as string,
+                pass: process.env.EMAIL_PASSWORD as string,
+            },
+            tls: {
+                rejectUnauthorized: false, // Disable certificate validation for self-signed certs
+            },
+        });
