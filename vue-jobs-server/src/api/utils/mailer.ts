268a64d82d2100d7fc50a3e4a7a1ab7b3c7ad4aa
moh123adan <maxamedadan169@gmail.com>
2024-10-04 23:36:29 +0300

    created function sending email

--- vue-jobs-server/src/api/utils/mailer.ts
+++ vue-jobs-server/src/api/utils/mailer.ts
@@ -18,3 +18,28 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false, // Disable certificate validation for self-signed certs
     },
 });
+
+
+// Function to send an email
+export const sendConfirmationEmail = async (email: string): Promise<void> => {
+    try {
+      const emailContent = `
+        <div style="font-family: Arial, sans-serif; background-color: #f8f8f8; padding: 20px;">
+          Email sent successfully
+        </div>
+      `;
+  
+      const mailOptions = {
+        from: process.env.EMAIL as string,
+        to: email,
+        subject: "Welcome Email",
+        html: emailContent,
+      };
+  
+      await transporter.sendMail(mailOptions);
+      console.log("Welcome email sent successfully");
+    } catch (error) {
+      console.error("Error sending welcome email:", error);
+      throw new Error("Failed to send welcome email.");
+    }
+  };
