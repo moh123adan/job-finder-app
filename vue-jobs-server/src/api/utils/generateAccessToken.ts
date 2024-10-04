3b33e6610c15a326732378255f744b295905536a
moh123adan <maxamedadan169@gmail.com>
2024-10-04 23:39:39 +0300

    Generate Access Token

--- vue-jobs-server/src/api/utils/generateAccessToken.ts
+++ vue-jobs-server/src/api/utils/generateAccessToken.ts
@@ -3,3 +3,9 @@ import jwt from "jsonwebtoken";
 
 // Define the type for the ID (adjust based on your actual ID type, e.g., string or number)
 type UserID = string | number;
+
+// Generate Access Token (No isAdmin)
+export const generateAccessToken = (id: UserID): string => {
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error("JWT_SECRET_KEY is not defined in the environment variables");
+    }
