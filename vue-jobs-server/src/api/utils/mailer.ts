80711f2dbe91007621d2dac2e7f3915c580a58d6
moh123adan <maxamedadan169@gmail.com>
2024-10-04 22:00:35 +0300

    Set up transporters

--- vue-jobs-server/src/api/utils/mailer.ts
+++ vue-jobs-server/src/api/utils/mailer.ts
@@ -2,3 +2,19 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+
+// Set up transporter
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL as string, // Your Gmail email
+        pass: process.env.EMAIL_PASSWORD as string, // The app password from Google
+    },
+    tls: {
+        rejectUnauthorized: false, // Disable certificate validation for self-signed certs
+    },
+});
