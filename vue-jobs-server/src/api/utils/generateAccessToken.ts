ab06039c560ebf953adbb82d418a8cc675be82a3
moh123adan <maxamedadan169@gmail.com>
2024-10-04 23:40:51 +0300

    Generate Refresh Token

--- vue-jobs-server/src/api/utils/generateAccessToken.ts
+++ vue-jobs-server/src/api/utils/generateAccessToken.ts
@@ -7,11 +7,24 @@ type UserID = string | number;
 // Generate Access Token (No isAdmin)
 export const generateAccessToken = (id: UserID): string => {
     if (!process.env.JWT_SECRET_KEY) {
-      throw new Error("JWT_SECRET_KEY is not defined in the environment variables");
+        throw new Error("JWT_SECRET_KEY is not defined in the environment variables");
     }
 
     const accessToken = jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
         expiresIn: "10m", // Access token expires in 10 minutes
-      });
-      return accessToken;
-    };
+    });
+    return accessToken;
+};
+
+// Generate Refresh Token (No isAdmin)
+export const generateRefreshToken = (id: UserID): string => {
+    if (!process.env.JWT_REFRESH_SECRET_KEY) {
+        throw new Error("JWT_REFRESH_SECRET_KEY is not defined in the environment variables");
+    }
+
+    const refreshToken = jwt.sign({ id }, process.env.JWT_REFRESH_SECRET_KEY, {
+        expiresIn: "7d", // Refresh token expires in 7 days
+    });
+    console.log("Refresh Token:", refreshToken); // Log the refresh token
+    return refreshToken;
+};
